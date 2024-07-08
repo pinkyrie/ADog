b8bd383081efcfc93203d7edaa601978a140ee71
pinkeyrye <3tanalien@gmail.com>
2024-07-08 21:16:39 +0800

    Initial commit

---
+++ widget.cpp
@@ -0,0 +1,16 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    setWindowTitle("ADog - Your App Usage Watch Dog");
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
